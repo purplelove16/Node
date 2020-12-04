282c207ab56e5c86e2bb89813e7866ecc46cfbcd
purplelove16 <apurva.jagannatha@gmail.com>
2020-12-04 17:48:06 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app = express();
+app.get('/', (req,res)=>{   
+    res.send("Welcome to the homepage....tqla");
+
+});
+app.listen(3000, () =>{
+    console.log("JSR....Whatzzappppp");
+});
