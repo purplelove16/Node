1dda4633ba1feeb1b0e5824f7da9d5345c9e537e
purplelove16 <apurva.jagannatha@gmail.com>
2020-12-04 19:31:03 +0000

    variable-port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.get('/', (req,res)=>{
     res.send("Welcome to the homepage....tqla");
 
 });
+const port = process.env.port || 3000;
 app.listen(3000, () =>{
     console.log("JSR....Whatzzappppp");
 });
